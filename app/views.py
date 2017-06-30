b7ddec359084324eb088594723b23e90e6a3eccd
simba999 <oliverking8985@yahoo.com>
2017-06-29 23:29:20 -0400

    generate metadata of xml

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ from tempfile import NamedTemporaryFile
 from datetime import datetime
 import PyPDF2
 import logging
+import time
 import pdb
 
 logger = logging.getLogger(__name__)
@@ -23,15 +24,24 @@ except ImportError:
 ZUGFERD_LEVEL = 'comfort'
 ZUGFERD_FILENAME = 'ZUGFeRD-invoice.xml'
 STATE = 'open'
-invoice = {
+INVOICE = {
     'state': 'open',
-    'number': '123456'
+    'number': '123456',
+    'comment': 'I like Illya because he is perfect guy',
+    'type': 'our_invoice'
 }
+root = None
 
-def _add_document_context_block(request, root, nsmap, ns):
+def _add_date(node_name, date_datetime, parent_node, ns):
+    date_node = etree.SubElement(parent_node, ns['ram'] + node_name)
+    date_node_str = etree.SubElement(
+        date_node, ns['udt'] + 'DateTimeString', format='102')
+    date_node_str.text = date_datetime
+
+def _add_document_context_block(root, nsmap, ns):
     doc_ctx = etree.SubElement(
         root, ns['rsm'] + 'SpecifiedExchangedDocumentContext')
-    if STATE not in ('open', 'paid'):
+    if INVOICE['state'] not in ('open', 'paid'):
         test_indic = etree.SubElement(doc_ctx, ns['ram'] + 'TestIndicator')
         indic = etree.SubElement(test_indic, ns['udt'] + 'Indicator')
         indic.text = 'true'
@@ -40,30 +50,28 @@ def _add_document_context_block(request, root, nsmap, ns):
     ctx_param_id = etree.SubElement(ctx_param, ns['ram'] + 'ID')
     ctx_param_id.text = '%s:%s' % (nsmap['rsm'], ZUGFERD_LEVEL)
 
-def _add_header_block(request, root, ns):
+def _add_header_block(root, ns):
         header_doc = etree.SubElement(
             root, ns['rsm'] + 'HeaderExchangedDocument')
         header_doc_id = etree.SubElement(header_doc, ns['ram'] + 'ID')
-        if STATE in ('open', 'paid'):
-            header_doc_id.text = self.number
+        if INVOICE['state'] in ('open', 'paid'):
+            header_doc_id.text = INVOICE['number']
         else:
-            header_doc_id.text = self.state
+            header_doc_id.text = INVOICE['state']
         header_doc_name = etree.SubElement(header_doc, ns['ram'] + 'Name')
-        if self.type == 'out_refund':
-            header_doc_name.text = _('Refund')
+        if INVOICE['type'] == 'out_refund':
+            header_doc_name.text = 'Refund'
         else:
-            header_doc_name.text = _('Invoice')
+            header_doc_name.text = 'Invoice'
         header_doc_typecode = etree.SubElement(
             header_doc, ns['ram'] + 'TypeCode')
         header_doc_typecode.text = '380'
-        # 380 = Commercial invoices (including refund)
-        date_invoice_dt = fields.Date.from_string(
-            self.date_invoice or fields.Date.context_today(self))
-        self._add_date('IssueDateTime', date_invoice_dt, header_doc, ns)
-        if self.comment:
+        date_invoice_dt = time.strftime("%Y%m%d")
+        _add_date('IssueDateTime', date_invoice_dt, header_doc, ns)
+        if INVOICE['comment']:
             note = etree.SubElement(header_doc, ns['ram'] + 'IncludedNote')
             content_note = etree.SubElement(note, ns['ram'] + 'Content')
-            content_note.text = self.comment
+            content_note.text = INVOICE['comment']
 
 # Create your views here.
 def generate_zugferd_xml(request):
@@ -87,8 +95,20 @@ def generate_zugferd_xml(request):
         }
     
     sign = 1
+
+    pdf_file = open('2.pdf', 'rb')
+    read_pdf = PdfFileReader(pdf_file)
+    number_of_pages = read_pdf.getNumPages()
+    
+    root = etree.Element(ns['rsm'] + 'CrossIndustryDocument', nsmap=nsmap)
+    
     _add_document_context_block(root, nsmap, ns)
-   _add_header_block(root, ns)
+    _add_header_block(root, ns)
+
+    xml_string = etree.tostring(
+        root, pretty_print=True, encoding='UTF-8', xml_declaration=True)
+    
+    print "&&&&& XML :   ", xml_string
 
     trade_transaction = etree.SubElement(
         root, ns['rsm'] + 'SpecifiedSupplyChainTradeTransaction')
@@ -97,20 +117,20 @@ def generate_zugferd_xml(request):
     # self._add_trade_delivery_block(trade_transaction, ns)
     # self._add_trade_settlement_block(trade_transaction, sign, ns)
 
-    line_number = 0
-    for iline in number_of_pages:
-        line_number += 1
-        self._add_invoice_line_block(
-            trade_transaction, iline, line_number, sign, ns)
+    # line_number = 0
+    # for iline in number_of_pages:
+    #     line_number += 1
+    #     self._add_invoice_line_block(
+    #         trade_transaction, iline, line_number, sign, ns)
 
-    xml_string = etree.tostring(
-        root, pretty_print=True, encoding='UTF-8', xml_declaration=True)
-    # self._check_xml_schema(
-    #     xml_string, 'account_invoice_factur-x/data/ZUGFeRD1p0.xsd')
-    logger.debug(
-        'ZUGFeRD XML file generated for invoice ID')
-    logger.debug(xml_string)
-    return xml_string
+    # xml_string = etree.tostring(
+    #     root, pretty_print=True, encoding='UTF-8', xml_declaration=True)
+    # # self._check_xml_schema(
+    # #     xml_string, 'account_invoice_factur-x/data/ZUGFeRD1p0.xsd')
+    # logger.debug(
+    #     'ZUGFeRD XML file generated for invoice ID')
+    # logger.debug(xml_string)
+    # return xml_string
     return HttpResponse("ok")
 
 def pdf_is_zugfered(request, filename):
