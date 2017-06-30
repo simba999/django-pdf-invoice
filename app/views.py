a96c92b2199df1e9353a415a0971ed593f60ce66
simba999 <oliverking8985@yahoo.com>
2017-06-30 09:42:48 -0400

    fix issues on generate xml

--- app/views.py
+++ app/views.py
@@ -28,7 +28,78 @@ INVOICE = {
     'state': 'open',
     'number': '123456',
     'comment': 'I like Illya because he is perfect guy',
-    'type': 'our_invoice'
+    'type': 'our_invoice',
+    'company_id': {
+        'name': 'IT Light',
+        'vat': '19',
+        'partner_id': {
+            'zip': '66032',
+            'street': 'street A',
+            'street2': 'Tower',
+            'country_id': 'US',
+            'city': 'New York'
+        }
+    },
+    'partner_id': {
+        'zip': '66032',
+        'street': 'street A',
+        'street2': 'Tower',
+        'country_id': 'US',
+        'city': 'New York'
+    },
+    'commercial_partner_id': {
+        'ref': 'comercial_partner_id_reference',
+        'name': 'Big Company',
+        'vat': '28'
+    },
+    'partner_bank_id': {
+        'bank_account_link': 'fixed',
+        'fixed_journal_id': {
+            'bank_account_id': '123457'
+        }
+    },
+    'currency_id': {
+        'name': 'eur',
+        'decimal_places': 3
+    },
+    'tax_line_ids': [
+        {
+            'tax_id': {
+                'unece_type_code': '',
+                'unece_categ_code': '',
+                'amount_type': 'kg'
+            },
+            'amount': 1,
+            'base': 1
+        },
+        {
+            'tax_id': {
+                'unece_type_code': '',
+                'unece_categ_code': '',
+                'amount_type': 'kg'
+            },
+            'amount': 1,
+            'base': 1
+        }
+    ],
+    'payment_mode_id': {
+        'note': 'infomation about payment mode',
+        'payment_method_id': {
+            'unece_code': '',
+            'name': ''
+        }
+    },
+    'payment_term_id': {
+        'name': 'paypal'
+    },
+    'fiscal_position_id': {
+        'note': 'information about fiscal postion'
+    },
+    'date_due': '2017213',
+    'amount_untaxed': 235,
+    'amount_tax': 120,
+    'amount_total': 375,
+    'residual': 56
 }
 root = None
 
@@ -38,29 +109,30 @@ def _add_date(node_name, date_datetime, parent_node, ns):
         date_node, ns['udt'] + 'DateTimeString', format='102')
     date_node_str.text = date_datetime
 
-def _add_address_block(self, partner, parent_node, ns):
+def _add_address_block(partner, parent_node, ns):
+    print "*** Partner ****: ", partner
     address = etree.SubElement(
         parent_node, ns['ram'] + 'PostalTradeAddress')
-    if partner.zip:
+    if partner['zip']:
         address_zip = etree.SubElement(
             address, ns['ram'] + 'PostcodeCode')
-        address_zip.text = partner.zip
-    if partner.street:
+        address_zip.text = partner['zip']
+    if partner['street']:
         address_street = etree.SubElement(
             address, ns['ram'] + 'LineOne')
-        address_street.text = partner.street
-        if partner.street2:
+        address_street.text = partner['street']
+        if partner['street2']:
             address_street2 = etree.SubElement(
                 address, ns['ram'] + 'LineTwo')
-            address_street2.text = partner.street2
-    if partner.city:
+            address_street2.text = partner['street2']
+    if partner['city']:
         address_city = etree.SubElement(
             address, ns['ram'] + 'CityName')
-        address_city.text = partner.city
-    if partner.country_id:
+        address_city.text = partner['city']
+    if partner['country_id']:
         address_country = etree.SubElement(
             address, ns['ram'] + 'CountryID')
-        address_country.text = partner.country_id.code
+        address_country.text = partner['country_id']
 
 def _add_trade_agreement_block(trade_transaction, ns):
     trade_agreement = etree.SubElement(
@@ -73,10 +145,10 @@ def _add_trade_agreement_block(trade_transaction, ns):
         seller, ns['ram'] + 'Name')
     seller_name.text = company['name']
     # Only with EXTENDED profile
-    # self._add_trade_contact_block(
-    #    self.user_id.partner_id or company.partner_id, seller, ns)
+    # INVOICE['_add_trade_contact_block(
+    #    INVOICE['user_id.partner_id or company.partner_id, seller, ns)
     _add_address_block(company['partner_id'], seller, ns)
-    if company.vat:
+    if company['vat']:
         seller_tax_reg = etree.SubElement(
             seller, ns['ram'] + 'SpecifiedTaxRegistration')
         seller_tax_reg_id = etree.SubElement(
@@ -92,9 +164,9 @@ def _add_trade_agreement_block(trade_transaction, ns):
         buyer, ns['ram'] + 'Name')
     buyer_name.text = INVOICE['commercial_partner_id']['name']
     # Only with EXTENDED profile
-    # if self.commercial_partner_id != self.partner_id:
-    #    self._add_trade_contact_block(
-    #        self.partner_id, buyer, ns)
+    # if INVOICE['commercial_partner_id != INVOICE['partner_id:
+    #    INVOICE['_add_trade_contact_block(
+    #        INVOICE['partner_id, buyer, ns)
     _add_address_block(INVOICE['partner_id'], buyer, ns)
     if INVOICE['commercial_partner_id']['vat']:
         buyer_tax_reg = etree.SubElement(
@@ -103,58 +175,181 @@ def _add_trade_agreement_block(trade_transaction, ns):
             buyer_tax_reg, ns['ram'] + 'ID', schemeID='VA')
         buyer_tax_reg_id.text = INVOICE['commercial_partner_id']['vat']
 
-    def _add_trade_delivery_block(trade_transaction, ns):
-        trade_agreement = etree.SubElement(
-            trade_transaction,
-            ns['ram'] + 'ApplicableSupplyChainTradeDelivery')
-        return trade_agreement
+def _add_trade_delivery_block(trade_transaction, ns):
+    trade_agreement = etree.SubElement(
+        trade_transaction,
+        ns['ram'] + 'ApplicableSupplyChainTradeDelivery')
+    return trade_agreement
 
-    def _add_trade_settlement_payment_means_block(request, trade_settlement, sign, ns):
-        payment_means = etree.SubElement(
-            trade_settlement,
-            ns['ram'] + 'SpecifiedTradeSettlementPaymentMeans')
-        payment_means_code = etree.SubElement(
-            payment_means, ns['ram'] + 'TypeCode')
-        payment_means_info = etree.SubElement(
-            payment_means, ns['ram'] + 'Information')
-        if INVOICE['payment_mode_id']:
-            payment_means_code.text = INVOICE['payment_mode_id']['payment_method_id']['unece_code']
-            payment_means_info.text = INVOICE['payment_mode_id']['note'] or INVOICE['payment_mode_id']['name']
-        else:
-            payment_means_code.text = '31'  # 31 = Wire transfer
-            payment_means_info.text = 'Wire transfer'
-            logger.warning(
-                'Missing payment mode on invoice ID %d. '
-                'Using 31 (wire transfer) as UNECE code as fallback '
-                'for payment mean',
-                INVOICE['id'])
-        if payment_means_code.text in ['31', '42']:
-            partner_bank = INVOICE['partner_bank_id']
-            if (
-                    not partner_bank and
-                    INVOICE['partner_bank_id'] and
-                    INVOICE['partner_bank_id']['bank_account_link'] == 'fixed' and
-                    INVOICE['partner_bank_id']['fixed_journal_id']):
-                partner_bank = INVOICE['partner_bank_id']['fixed_journal_id']['bank_account_id']
-            if partner_bank and partner_bank.acc_type == 'iban':
-                payment_means_bank_account = etree.SubElement(
+def _add_trade_settlement_block(trade_transaction, sign, ns):
+    inv_currency_name = INVOICE['currency_id']['name']
+    prec = INVOICE['currency_id']['decimal_places']
+    trade_settlement = etree.SubElement(
+        trade_transaction,
+        ns['ram'] + 'ApplicableSupplyChainTradeSettlement')
+    payment_ref = etree.SubElement(
+        trade_settlement, ns['ram'] + 'PaymentReference')
+    payment_ref.text = INVOICE['number'] or INVOICE['state']
+    invoice_currency = etree.SubElement(
+        trade_settlement, ns['ram'] + 'InvoiceCurrencyCode')
+    invoice_currency.text = inv_currency_name
+    if (
+            INVOICE['payment_mode_id'] and
+            not INVOICE['payment_mode_id']['payment_method_id']['unece_code']):
+        try:
+            raise Exception("Missing UNECE code on payment export type '%s'" % INVOICE['payment_mode_id']['payment_method_id']['name'])
+        except Exception as error:
+            print('caught this error: ' + repr(error))
+
+    if (
+            INVOICE['type'] == 'out_invoice' or
+            (INVOICE['payment_mode_id'] and
+                INVOICE['payment_mode_id']['payment_method_id']['unece_code']
+                not in [31, 42])):
+        _add_trade_settlement_payment_means_block(trade_settlement, sign, ns)
+    tax_basis_total = 0.0
+    if INVOICE['tax_line_ids']:
+        for tline in INVOICE['tax_line_ids']:
+            tax = tline['tax_id']
+            if not tax['unece_type_code']:
+                try:
+                    raise Exception("Missing UNECE Tax Type on tax '%s'" % tax['name'])
+                except Exception as error:
+                    print('caught this error: ' + repr(error))
+
+            if not tax['unece_categ_code']:
+                try:
+                    raise Exception("Missing UNECE Tax Category on tax '%s'" % tax['name'])
+                except Exception as error:
+                    print('caught this error: ' + repr(error))
+                
+            trade_tax = etree.SubElement(
+                trade_settlement, ns['ram'] + 'ApplicableTradeTax')
+            amount = etree.SubElement(
+                trade_tax, ns['ram'] + 'CalculatedAmount',
+                currencyID=inv_currency_name)
+            amount.text = unicode(tline['amount'] * sign)
+            tax_type = etree.SubElement(
+                trade_tax, ns['ram'] + 'TypeCode')
+            tax_type.text = tax['unece_categ_code']
+
+            # if (
+            #         tax.unece_categ_code != 'S' and
+            #         float_is_zero(tax.amount, precision_digits=prec) and
+            #         INVOICE['fiscal_position_id'] and
+            #         INVOICE['fiscal_position_id']['note']):
+            #     exemption_reason = etree.SubElement(
+            #         trade_tax, ns['ram'] + 'ExemptionReason')
+            #     exemption_reason.text = with_context(
+            #         lang=INVOICE['partner_id']['lang'] or 'en_US').\
+            #         fiscal_position_id['note']
+
+            base = etree.SubElement(
+                trade_tax,
+                ns['ram'] + 'BasisAmount', currencyID=inv_currency_name)
+            base.text = unicode(tline['base'] * sign)
+            tax_basis_total += tline['base']
+            tax_categ_code = etree.SubElement(
+                trade_tax, ns['ram'] + 'CategoryCode')
+            tax_categ_code.text = tax['unece_categ_code']
+            if tax['amount_type'] == 'percent':
+                percent = etree.SubElement(
+                    trade_tax, ns['ram'] + 'ApplicablePercent')
+                percent.text = unicode(tax.amount)
+    trade_payment_term = etree.SubElement(
+        trade_settlement, ns['ram'] + 'SpecifiedTradePaymentTerms')
+    trade_payment_term_desc = etree.SubElement(
+        trade_payment_term, ns['ram'] + 'Description')
+    # The 'Description' field of SpecifiedTradePaymentTerms
+    # is a required field, so we must always give a value
+    if INVOICE['payment_term_id']:
+        trade_payment_term_desc.text = INVOICE['payment_term_id']['name']
+    else:
+        trade_payment_term_desc.text = 'No specific payment term selected'
+
+    if INVOICE['date_due']:
+        date_due_dt = INVOICE['date_due']
+        _add_date('DueDateDateTime', date_due_dt, trade_payment_term, ns)
+
+    sums = etree.SubElement(
+        trade_settlement,
+        ns['ram'] + 'SpecifiedTradeSettlementMonetarySummation')
+    line_total = etree.SubElement(
+        sums, ns['ram'] + 'LineTotalAmount', currencyID=inv_currency_name)
+    line_total.text = '%0.*f' % (prec, INVOICE['amount_untaxed'] * sign)
+    charge_total = etree.SubElement(
+        sums, ns['ram'] + 'ChargeTotalAmount',
+        currencyID=inv_currency_name)
+    charge_total.text = '0.00'
+    allowance_total = etree.SubElement(
+        sums, ns['ram'] + 'AllowanceTotalAmount',
+        currencyID=inv_currency_name)
+    allowance_total.text = '0.00'
+    tax_basis_total_amt = etree.SubElement(
+        sums, ns['ram'] + 'TaxBasisTotalAmount',
+        currencyID=inv_currency_name)
+    tax_basis_total_amt.text = '%0.*f' % (prec, tax_basis_total * sign)
+    tax_total = etree.SubElement(
+        sums, ns['ram'] + 'TaxTotalAmount', currencyID=inv_currency_name)
+    tax_total.text = '%0.*f' % (prec, INVOICE['amount_tax'] * sign)
+    total = etree.SubElement(
+        sums, ns['ram'] + 'GrandTotalAmount', currencyID=inv_currency_name)
+    total.text = '%0.*f' % (prec, INVOICE['amount_total'] * sign)
+    prepaid = etree.SubElement(
+        sums, ns['ram'] + 'TotalPrepaidAmount',
+        currencyID=inv_currency_name)
+    residual = etree.SubElement(
+        sums, ns['ram'] + 'DuePayableAmount', currencyID=inv_currency_name)
+    prepaid.text = '%0.*f' % (
+        prec, (INVOICE['amount_total'] - INVOICE['residual']) * sign)
+    residual.text = '%0.*f' % (prec, INVOICE['residual'] * sign)
+
+def _add_trade_settlement_payment_means_block(trade_settlement, sign, ns):
+    payment_means = etree.SubElement(
+        trade_settlement,
+        ns['ram'] + 'SpecifiedTradeSettlementPaymentMeans')
+    payment_means_code = etree.SubElement(
+        payment_means, ns['ram'] + 'TypeCode')
+    payment_means_info = etree.SubElement(
+        payment_means, ns['ram'] + 'Information')
+    if INVOICE['payment_mode_id']:
+        payment_means_code.text = INVOICE['payment_mode_id']['payment_method_id']['unece_code']
+        payment_means_info.text = INVOICE['payment_mode_id']['note'] or INVOICE['payment_mode_id']['name']
+    else:
+        payment_means_code.text = '31'  # 31 = Wire transfer
+        payment_means_info.text = 'Wire transfer'
+        logger.warning(
+            'Missing payment mode on invoice ID %d. '
+            'Using 31 (wire transfer) as UNECE code as fallback '
+            'for payment mean',
+            INVOICE['id'])
+    if payment_means_code.text in ['31', '42']:
+        partner_bank = INVOICE['partner_bank_id']
+        if (
+                not partner_bank and
+                INVOICE['partner_bank_id'] and
+                INVOICE['partner_bank_id']['bank_account_link'] == 'fixed' and
+                INVOICE['partner_bank_id']['fixed_journal_id']):
+            partner_bank = INVOICE['partner_bank_id']['fixed_journal_id']['bank_account_id']
+        if partner_bank and partner_bank.acc_type == 'iban':
+            payment_means_bank_account = etree.SubElement(
+                payment_means,
+                ns['ram'] + 'PayeePartyCreditorFinancialAccount')
+            iban = etree.SubElement(
+                payment_means_bank_account, ns['ram'] + 'IBANID')
+            iban.text = partner_bank.sanitized_acc_number
+            if partner_bank.bank_bic:
+                payment_means_bank = etree.SubElement(
                     payment_means,
-                    ns['ram'] + 'PayeePartyCreditorFinancialAccount')
-                iban = etree.SubElement(
-                    payment_means_bank_account, ns['ram'] + 'IBANID')
-                iban.text = partner_bank.sanitized_acc_number
-                if partner_bank.bank_bic:
-                    payment_means_bank = etree.SubElement(
-                        payment_means,
-                        ns['ram'] +
-                        'PayeeSpecifiedCreditorFinancialInstitution')
-                    payment_means_bic = etree.SubElement(
-                        payment_means_bank, ns['ram'] + 'BICID')
-                    payment_means_bic.text = partner_bank.bank_bic
-                    if partner_bank.bank_name:
-                        bank_name = etree.SubElement(
-                            payment_means_bank, ns['ram'] + 'Name')
-                        bank_name.text = partner_bank.bank_name
+                    ns['ram'] +
+                    'PayeeSpecifiedCreditorFinancialInstitution')
+                payment_means_bic = etree.SubElement(
+                    payment_means_bank, ns['ram'] + 'BICID')
+                payment_means_bic.text = partner_bank.bank_bic
+                if partner_bank.bank_name:
+                    bank_name = etree.SubElement(
+                        payment_means_bank, ns['ram'] + 'Name')
+                    bank_name.text = partner_bank.bank_name
 
 def _add_document_context_block(root, nsmap, ns):
     doc_ctx = etree.SubElement(
@@ -238,12 +433,12 @@ def generate_zugferd_xml(request):
     # line_number = 0
     # for iline in number_of_pages:
     #     line_number += 1
-    #     self._add_invoice_line_block(
+    #     INVOICE['_add_invoice_line_block(
     #         trade_transaction, iline, line_number, sign, ns)
 
     # xml_string = etree.tostring(
     #     root, pretty_print=True, encoding='UTF-8', xml_declaration=True)
-    # # self._check_xml_schema(
+    # # INVOICE['_check_xml_schema(
     # #     xml_string, 'account_invoice_factur-x/data/ZUGFeRD1p0.xsd')
     # logger.debug(
     #     'ZUGFeRD XML file generated for invoice ID')
@@ -265,7 +460,6 @@ def pdf_is_zugfered(request, filename):
     pdb.set_trace()
     is_zugferd = False
     try:
-        
         fd = StringIO(pdf_content)
         pdf = PdfFileReader(fd)
         pdf_root = pdf.trailer['/Root']
