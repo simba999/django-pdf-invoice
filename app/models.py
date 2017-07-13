e4bf602bed0619e57fef6684223d1bc9946065ee
simba999 <oliverking8985@yahoo.com>
2017-07-13 08:59:24 -0400

    add generting pdf invoice file module

--- app/models.py
+++ app/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+from addressbook.models import Address
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    addresses = models.ManyToManyField(Address)
+
+    def __unicode__(self):
+        return self.user.email
+
+    @property
+    def address(self):
+        return self.addresses.latest()
